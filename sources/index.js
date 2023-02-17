340a21c3a54e467ea4a0e0cc36b5b09424e0acb3
oti ayjay <otiedwin40@gmail.com>
2023-02-17 16:55:26 +0100

    a few changes and incorporate js

--- sources/index.js
+++ sources/index.js
@@ -1,11 +1,15 @@
-let toggler = document.getElementById('toggler'),
-    toggled = document.getElementById('toggle').style.display
+var toggler = document.getElementById('toggler')
 
 function toggle(){
-   if(toggled == "flex"){
-      toggled = "none";
-   }else{
-      toggled = "flex"
+   var toggled = document.getElementById('toggle').style.display
+
+   if(toggled == "none"){
+      console.log(`changing '${toggled} to 'flex'`)
+      document.getElementById('toggle').style = "display : flex"
+   }
+   else{
+      console.log(`changing '${toggled} to 'none'`)
+      document.getElementById('toggle').style = "display : none"
    }
 }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
 toggler.addEventListener('click', toggle)          
