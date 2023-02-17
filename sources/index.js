f4d0bb8253afb225840efa56f4678fa5cae5247c
oti ayjay <otiedwin40@gmail.com>
2023-02-17 12:29:33 +0100

    porting all my latest changes

--- sources/index.js
+++ sources/index.js
@@ -1,1 +1,11 @@
-function toggle(){}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+let toggler = document.getElementById('toggler'),
+    toggled = document.getElementById('toggle').style.display
+
+function toggle(){
+   if(toggled == "flex"){
+      toggled = "none";
+   }else{
+      toggled = "flex"
+   }
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+toggler.addEventListener('click', toggle)          
